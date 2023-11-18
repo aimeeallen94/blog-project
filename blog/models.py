9b11a48ed9288a7e0361643b48e6b4051adeab5f
Aimee Allen <aimeeallen94@gmail.com>
2023-11-18 14:41:48 +0000

    setting up comment and posting option

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -10,15 +12,15 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
-        User, related_name='blog_likes', blank=True)
-    
+        User, related_name='blogpost_like', blank=True)
+
     class Meta:
         ordering = ["-created_on"]
 
@@ -28,9 +30,10 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name='comments')
+                             related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
